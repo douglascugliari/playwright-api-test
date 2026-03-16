8365ec1db9d4127ba28d97600ee76d898529ff23
douglascugliarisenai <douglas_s5@estudante.sc.senai.br>
2026-03-16 20:36:20 -0300

    fix: 🐛 refatoração nos testes

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -13,39 +13,32 @@ test.describe("Login", () => {
     });
 
     test("Deve fazer login com sucesso", async ({ pegarUsuarioPorId }) => {
-        await allure.description("Este teste verifica se é possível fazer login com sucesso");
-        await allure.feature("Login");
-        await allure.tags("login", "sucesso");
         const { email, password } = pegarUsuarioPorId;
         const response = await loginApi.fazerLogin(email, password);
-        expect(response.status()).toBe(200);
         const responseBody = await response.json();
         await allure.attachment('response', JSON.stringify(responseBody, null, 2), 'application/json');
+
         expect(responseBody.authorization).toBeDefined();
         expect(responseBody.message).toBe("Login realizado com sucesso");
     });
 
     test("Deve falhar login com senha incorreta", async ({ pegarUsuarioPorId }) => {
-        await allure.description("Este teste verifica se é possível falhar login com senha incorreta");
-        await allure.feature("Login");
-        await allure.tags("login", "falha");
         const { email } = pegarUsuarioPorId;
         const response = await loginApi.fazerLogin(email, "senha-incorreta");
-        expect(response.status()).toBe(401);
         const responseBody = await response.json();
         await allure.attachment('response', JSON.stringify(responseBody, null, 2), 'application/json');
+
+        expect(response.status()).toBe(401);
         expect(responseBody.message).toBe("Email e/ou senha inválidos");
     });
 
     test("Deve falhar login com email não cadastrado", async () => {
-        await allure.description("Este teste verifica se é possível falhar login com email não cadastrado");
-        await allure.feature("Login");
-        await allure.tags("login", "falha");
         const { email, password } = UsuarioFactory.criarUsuario();
         const response = await loginApi.fazerLogin(email, password);
-        expect(response.status()).toBe(401);
         const responseBody = await response.json();
         await allure.attachment('response', JSON.stringify(responseBody, null, 2), 'application/json');
+
+        expect(response.status()).toBe(401);
         expect(responseBody.message).toBe("Email e/ou senha inválidos");
     });
 });
--- tests/usuarios.spec.ts
+++ tests/usuarios.spec.ts
@@ -12,52 +12,40 @@ test.describe("Usuários", () => {
     });
 
     test("CT-001 - Deve criar um usuário", async () => {
-        await allure.description("Este teste verifica se é possível criar um usuário");
-        await allure.feature("Usuários");
-        await allure.tags("usuarios", "criacao");
         const response = await usuariosApi.criarUsuario();
-        await allure.attachment('response', JSON.stringify(response, null, 2), 'application/json');
+        await allure.attachment('response', JSON.stringify(await response.json(), null, 2), 'application/json');
 
         expect(response.status()).toBe(201);
     });
 
     test("CT-002 - Deve listar usuários", async () => {
-        await allure.description("Este teste verifica se é possível listar usuários");
-        await allure.feature("Usuários");
-        await allure.tags("usuarios", "listagem");
         const response = await usuariosApi.listarUsuarios();
-        await allure.attachment('response', JSON.stringify(response, null, 2), 'application/json');
-        expect(response.ok()).toBeTruthy();
-
         const usuarios = await response.json();
+        await allure.attachment('response', JSON.stringify(usuarios, null, 2), 'application/json');
+
+        expect(response.ok()).toBeTruthy();
         expect(usuarios.usuarios.length).toBeGreaterThan(0);
     });
 
     test("CT-003 - Deve obter um usuário por ID", async ({ criarUsuarioFixture }) => {
-        await allure.description("Este teste verifica se é possível obter um usuário por ID");
-        await allure.feature("Usuários");
-        await allure.tags("usuarios", "busca");
         const response = await usuariosApi.obterUsuarioPorId(criarUsuarioFixture._id);
-        await allure.attachment('response', JSON.stringify(response, null, 2), 'application/json');
+        await allure.attachment('response', JSON.stringify(await response.json(), null, 2), 'application/json');
+
         expect(response.ok()).toBeTruthy();
     });
 
     test("CT-004 - Deve atualizar um usuário", async ({ criarUsuarioFixture }) => {
-        await allure.description("Este teste verifica se é possível atualizar um usuário");
-        await allure.feature("Usuários");
-        await allure.tags("usuarios", "edicao");
         const usuarioAtualizado = UsuarioFactory.criarUsuario();
         const response = await usuariosApi.atualizarUsuario(criarUsuarioFixture._id, usuarioAtualizado);
-        await allure.attachment('response', JSON.stringify(response, null, 2), 'application/json');
+        await allure.attachment('response', JSON.stringify(await response.json(), null, 2), 'application/json');
+
         expect(response.ok()).toBeTruthy();
     });
 
     test("CT-005 - Deve deletar um usuário", async ({ criarUsuarioFixture }) => {
-        await allure.description("Este teste verifica se é possível deletar um usuário");
-        await allure.feature("Usuários");
-        await allure.tags("usuarios", "delecao");
         const response = await usuariosApi.deletarUsuario(criarUsuarioFixture._id);
-        await allure.attachment('response', JSON.stringify(response, null, 2), 'application/json');
+        await allure.attachment('response', JSON.stringify(await response.json(), null, 2), 'application/json');
+
         expect(response.ok()).toBeTruthy();
     });
 });
